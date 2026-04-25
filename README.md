5dc940095c51bff0096aac7655dc949d8d72c39b
Ekaterina Nikulnikova <nikulnikowa2014@gmail.com>
2026-04-25 20:41:22 +0800

    реализация проекта

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/knikulnikova/smart-table
 # sp6-2_smart-table_starter
--- src/components/table.js
+++ src/components/table.js
@@ -11,13 +11,42 @@ export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    // #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {               // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+    root.container.prepend(root[subName].container);   // добавляем к таблице после (append) или до (prepend)
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    after.forEach(subName => {  
+    root[subName] = cloneTemplate(subName);
+    root.container.append(root[subName].container);
+    });
+
+    console.log(root);
+
+    // #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', (e) => {
+        onAction();
+    });
+    root.container.addEventListener('reset', (e) => {
+        setTimeout(onAction, 1000);
+    });
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        // #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+        const nextRows = data.map(item => { 
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => { 
+                if (row.elements[key]) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
