bee86a612c5a218eb0826aee016e098da52336b2
Ekaterina Nikulnikova <nikulnikowa2014@gmail.com>
2026-05-13 11:56:09 +0800

    убрана data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
-import {data as sourceData} from "./data/dataset_1.js";
+// import {data as sourceData} from "./data/dataset_1.js";
 
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
@@ -15,7 +15,7 @@ import {initSearching} from "./components/searching.js";
 
 
 // Исходные данные используемые в render()
-const api = initData(sourceData);
+const api = initData();
 
 /**
  * Сбор и обработка полей из таблицы
